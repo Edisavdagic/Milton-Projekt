ea244fd04490fa9048d694d2491cbc0e34d8f23d
Edisavdagic <119938495+Edisavdagic@users.noreply.github.com>
2026-03-21 16:58:20 +0100

    opsætning af sass
    variables, mixins, main

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,7 +4,6 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 
 function login() {
-  // Her kan du senere tilføje rigtig login logik
   router.push('/dashboard')
 }
 </script>
@@ -14,29 +13,36 @@ function login() {
     <h1>Login</h1>
 
     <form @submit.prevent="login">
-      <input type="text" placeholder="Brugernavn" required />
-      <input type="password" placeholder="Adgangskode" required />
 
-      <button type="submit">Log ind</button>
     </form>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use "../assets/styles/variables" as *;
+
 .login-container {
   max-width: 300px;
   margin: 100px auto;
   display: flex;
   flex-direction: column;
   gap: 1rem;
+
+  h1 {
+    color: $primary;
+  }
 }
 
 input {
   padding: 0.5rem;
+  border: 1px solid $secondary;
 }
 
 button {
   padding: 0.5rem;
   cursor: pointer;
+  color: white;
+  background-color: $primary;
 }
 </style>
+
