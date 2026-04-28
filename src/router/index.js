2273a6c0199e2934912b2d0a2aee2536ee0a7ae2
Victor Brøndum <vbroendum@gmail.com>
2026-04-28 10:37:41 +0200

    removes improper indentation in imports

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-  import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
-import DashboardView from '../views/DashboardView.vue'
-import DocumentsView from '../views/DocumentsView.vue'
-import ProjectoverviewView from '../views/ProjectoverView.vue'
-import NotificationsView from '@/views/NotificationsView.vue'
-import { useAuthStore } from '@/stores/auth'
-import CalenderView from '@/views/CalenderView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "../views/LoginView.vue";
+import DashboardView from "../views/DashboardView.vue";
+import DocumentsView from "../views/DocumentsView.vue";
+import ProjectoverviewView from "../views/ProjectoverView.vue";
+import NotificationsView from "@/views/NotificationsView.vue";
+import { useAuthStore } from "@/stores/auth";
+import CalenderView from "@/views/CalenderView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
