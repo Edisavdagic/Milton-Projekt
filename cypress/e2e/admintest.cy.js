235b6a88870be4a18ea0d70f5fd4af1b9e80099f
Victor Brøndum <vbroendum@gmail.com>
2026-05-10 00:59:46 +0200

    Remove oxlint and fix linting configs

--- cypress/e2e/admintest.cy.js
+++ cypress/e2e/admintest.cy.js
@@ -2,23 +2,23 @@
 
 describe('Login flow', () => {
   it('viser login siden korrekt', () => {
-    cy.visit('/')
+    cy.visit('/');
 
-    cy.get('input[type="email"]').should('be.visible')
-    cy.get('input[type="password"]').should('be.visible')
-    cy.contains('Log ind').should('be.visible')
-  })
-})
+    cy.get('input[type="email"]').should('be.visible');
+    cy.get('input[type="password"]').should('be.visible');
+    cy.contains('Log ind').should('be.visible');
+  });
+});
 
 describe('Login som admin', () => {
   it('logger ind og går til projektoversigt', () => {
-    cy.visit('/')
+    cy.visit('/');
 
-    cy.get('input[type="email"]').type('testadmin@test.dk')
-    cy.get('input[type="password"]').type('test1234')
+    cy.get('input[type="email"]').type('testadmin@test.dk');
+    cy.get('input[type="password"]').type('test1234');
 
-    cy.contains('Log ind').click()
+    cy.contains('Log ind').click();
 
-    cy.url().should('include', '/projektoversigt')
-  })
-})
+    cy.url().should('include', '/projektoversigt');
+  });
+});
--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import globals from 'globals'
-import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import pluginCypress from 'eslint-plugin-cypress'
-import pluginVitest from '@vitest/eslint-plugin'
-import pluginOxlint from 'eslint-plugin-oxlint'
-import plugin from 'eslint-plugin-cypress'
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import pluginCypress from 'eslint-plugin-cypress';
+import pluginVitest from '@vitest/eslint-plugin';
 
 export default defineConfig([
   {
@@ -13,7 +11,13 @@ export default defineConfig([
     files: ['**/*.{vue,js,mjs,jsx}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  globalIgnores([
+    '**/dist/**',
+    '**/dist-ssr/**',
+    '**/coverage/**',
+    'docs/**',
+    'themes/**',
+  ]),
 
   {
     languageOptions: {
@@ -25,14 +29,10 @@ export default defineConfig([
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  ...plugin.configs["flat/strongly-recommended"],
 
   {
     ...pluginCypress.configs.recommended,
-    files: [
-      'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/support/**/*.{js,ts,jsx,tsx}',
-    ],
+    files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
   },
 
   {
@@ -40,34 +40,31 @@ export default defineConfig([
     files: ['src/**/__tests__/*'],
   },
 
-  ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
-
   {
     rules: {
-      'comma-dangle': ["error", "always-multiline"],
-      'eqeqeq': ["error", "always"],
-      'indent': ["error", 2],
-      'no-eval': ["error"],
-      'no-trailing-spaces': ["error"],
-      'no-unused-vars': ["error"],
-      'no-var': ["error"],
-      'prefer-const': ["error"],
-      'quotes': ["error", "single"],
-      'semi': ["error", "always"],
-      'no-undef': ["error"],
-      'no-console': ["warn"],
-      'consistent-return': ["error"],
-      'no-mixed-spaces-and-tabs': ["warning"],
-      'no-multiple-empty-lines': ["warning", { "max": 1 }],
-      'no-multi-spaces': ["warning"],
-      'no-lonely-if': ["warning"],
-      'camelcase': ["error", { "properties": "always" }],
-      'vue/no-unused-vars': ["warn"],
-      'vue/no-unused-components': ["warn"],
-      'vue/html-end-tags': ["error"],
-      'vue/no-lone-template': ["error"],
-      'vue/no-use-v-if-with-v-for ': ["error"],
+      'comma-dangle': ['error', 'always-multiline'],
+      eqeqeq: ['error', 'always'],
+      indent: ['error', 2],
+      'no-eval': ['error'],
+      'no-trailing-spaces': ['error'],
+      'no-unused-vars': ['error'],
+      'no-var': ['error'],
+      'prefer-const': ['error'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'no-undef': ['error'],
+      'no-console': ['warn'],
+      'consistent-return': ['error'],
+      'no-mixed-spaces-and-tabs': ['warn'],
+      'no-multiple-empty-lines': ['warn', { max: 1 }],
+      'no-multi-spaces': ['warn'],
+      'no-lonely-if': ['warn'],
+      camelcase: ['error', { properties: 'always' }],
+      'vue/no-unused-vars': ['warn'],
+      'vue/no-unused-components': ['warn'],
+      'vue/html-end-tags': ['error'],
+      'vue/no-lone-template': ['error'],
+      'vue/no-use-v-if-with-v-for': ['error'],
     },
-  }
-  
-])
+  },
+]);
